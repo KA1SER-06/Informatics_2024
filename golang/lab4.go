c3d47bb611ccb634ad1c32d97f548709d11556e8
KA1SER-06 <aspinfernus@gmail.com>
2024-10-26 11:59:41 +0300

    added 4lab

--- golang/lab4.go
+++ golang/lab4.go
@@ -1,4 +1,4 @@
-package main
+package lab4
 
 import (
 "fmt"
@@ -6,22 +6,53 @@ import (
 )
 
 func calculateY(a, x float64) float64 {
-// Вычисляем выражения
 xSquaredMinus1 := x*x - 1
-part1 := math.Pow(a, xSquaredMinus1) // a^(x^2 - 1)
-part2 := math.Log(xSquaredMinus1) // ln(x^2 - 1)
-part3 := math.Cbrt(xSquaredMinus1) // корень кубический (x^2 - 1)
+part1 := math.Pow(a, xSquaredMinus1) 
+part2 := math.Log(xSquaredMinus1) 
+part3 := math.Cbrt(xSquaredMinus1) 
 
-// Итоговое значение y
 y := part1 - part2 + part3
 return y
 }
 
-func main() {
+func SolveTaskA(a, b, xStart, xEnd, step float64) []float64 {
+	results := []float64{}
+	for x := xStart; x <= xEnd; x += step {
+		y := CalculateY(a, b, x)
+		results = append(results, y)
+	}
+	return results
+}
+
+func SolveTaskB(a, b float64, xValues []float64) []float64 {
+	results := []float64{}
+	for _, x := range xValues {
+		y := CalculateY(a, b, x)
+		results = append(results, y)
+	}
+	return results
+}
+
+func startLab4() {
 a := 1.6
-x1 := 1.28
 
-y := calculateY(a, x1)
+fmt.Println("Задача A:")
+	xStartA := 1.2
+	xEndA := 3.7
+	step := 0.5
 
-fmt.Printf("Значение y для x1=%.2f: %.4f\n", x1, y)
+	for x := xStartA; x <= xEndA; x += step {
+		y := calculateY(a, b, x)
+		fmt.Printf("x = %.2f, y = %.5f\n", x, y)
+	}
+
+
+	fmt.Println("Задача B:")
+	xValuesB := []float64{1.28, 1.36, 2.47, 3.68, 4.56}
+
+
+	for _, x := range xValuesB {
+		y := calculateY(a, b, x)
+		fmt.Printf("x = %.2f, y = %.5f\n", x, y)
+	}
 }
--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	lab4 "Git ISUCT/Informatics_2024/golang/lab-4.go"
+)
 
 func main() {
 	fmt.Println("Максимов Даниил Андреевич")
+	lab4.startLab4()
 }
