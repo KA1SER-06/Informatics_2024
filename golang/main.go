a8796acb0b3d8cabe49379c257f66d3474d3fcf9
Maksimov Daniil <aspinfernus@gmail.com>
2024-12-28 12:58:18 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -10,9 +10,8 @@ func main() {
 	fmt.Println("Maksimov_Daniil_Andreevich")
 
 	lab6.Lab6()
-=======
 	lab4 "isuct.ru/informatics2022/Lab4"
-)
+}
 
 func main() {
 	fmt.Println("Maksimov Daniil Andreevich")
